0bfb2969b7a48480b2789301e4eed93625746296
amagsty <amagsty@sina.cn>
2024-02-07 00:18:53 +0800

    变更版本号

--- include/main.h
+++ include/main.h
@@ -2,7 +2,7 @@
 #define _MAIN_H
 
 #define PROJECT_NANE "aDataFlow"
-#define FIRMWARE_VERSION "2.1.0"
+#define FIRMWARE_VERSION "1.2.1"
 #define HARDWARE_VERSION_PCB_MAIN ">=2.5"
 #define HARDWARE_VERSION_PCB_FRONT ">=2.3"
 #define HARDWARE_VERSION_PCB_CONN ">=2.4"
