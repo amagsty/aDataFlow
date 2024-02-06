1c997712102a60e470cf28b07cbeb70f2b5b7ad8
amagsty <amagsty@sina.cn>
2024-02-06 12:15:54 +0800

    修改部分变量

--- src/buttons.cpp
+++ src/buttons.cpp
@@ -2,24 +2,24 @@
 
 static bool is_chart = 0;
 
-static OneButton hw_btn_chart(PIN_BTN_CHART, true);
+static OneButton hw_btn_mode(PIN_BTN_CHART, true);
 static OneButton hw_btn_rec(PIN_BTN_REC, true);
 
 static void task_buttons(void *pvParameters)
 {
     for (;;)
     {
-        hw_btn_chart.tick();
+        hw_btn_mode.tick();
         hw_btn_rec.tick();
-        vTaskDelay(100);
+        vTaskDelay(50);
     }
     vTaskDelete(NULL);
 }
 
 static void toggle_chart(void)
 {
-    toggle_chart_terminal = !toggle_chart_terminal;
-    log_d("toggle chart: %d", toggle_chart_terminal);
+    is_mode_changing = !is_mode_changing;
+    log_d("is_mode_changing: %d", is_mode_changing);
     // ui_toggle_chart_terminal();
 }
 
@@ -31,7 +31,7 @@ static void toggle_rec(void)
 
 void buttons_init(void)
 {
-    hw_btn_chart.attachClick(toggle_chart);
+    hw_btn_mode.attachClick(toggle_chart);
     hw_btn_rec.attachClick(toggle_rec);
     xTaskCreatePinnedToCore(task_buttons, "task_buttons", 2048, NULL, 1, NULL, 0);
 }
